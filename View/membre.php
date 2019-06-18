bf39d766ef92f3051e1920477d4033f2bc64f675
leocarel <leocar30@gmail.com>
2019-06-18 14:11:33 +0200

    Update membre.php

--- View/membre.php
+++ View/membre.php
@@ -1,7 +1,7 @@
 <?php
     include_once "header.php" ; 
 ?>
-    <a href="./index.php?ctrl=membre&action=infoPerso">
+ <a href="./index.php?ctrl=membre&action=infoPerso"> Espace membre lol vroom vroom </a> 
 <?php
     include_once "header.php" ; 
 ?>
