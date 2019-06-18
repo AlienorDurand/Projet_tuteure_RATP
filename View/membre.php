d95e2702fd623591497a589b6f41dbb9d309bbd1
leocarel <leocar30@gmail.com>
2019-06-18 14:09:22 +0200

    Update membre.php

--- View/membre.php
+++ View/membre.php
@@ -1,9 +1,7 @@
 <?php
-    include_once "header.php" 
+    include_once "header.php" ; 
 ?>
-
-
-
+    <a href="./index.php?ctrl=membre&action=infoPerso">
 <?php
-    include_once "header.php" 
+    include_once "header.php" ; 
 ?>
