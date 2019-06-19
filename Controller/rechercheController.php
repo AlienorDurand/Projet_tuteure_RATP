34ed7f1fd6ab98028f8018849ee249c1f32fcf7c
marcantoine-g <marc-antoine.g@outlook.com>
2019-06-19 16:50:41 +0200

    Ajout des variables 'depart' et 'arrivee' issues du formulaire

--- Controller/rechercheController.php
+++ Controller/rechercheController.php
@@ -1,8 +1,14 @@
 <?php
 
 class rechercheController{
+    private $depart;
+    private $arrivee;
 
     public function recherchePage(){
+        if((isset($_POST['depart']) && !empty($_POST['depart'])) && (isset($_POST['arrivee']) && !empty($_POST['arrivee']))) {          
+           $depart = $_POST['depart'];
+           $arrivee = $_POST['arrivee'];
+        }
        require('./View/recherche.php'); 
     }
     
--- View/recherche.php
+++ View/recherche.php
@@ -1,14 +1,53 @@
 <?php
-    include_once "header.php" 
+    include_once "header.php" ;
 ?>
     <div class="row">
         <div id="partierecherche" class="col-md-12">
-        
-        
-
+             <?php echo $depart ?>  
+    
         </div>
     </div>
 
 <?php 
     include_once "footer.php"
 ?>
+
+<script>
+
+    const trajets = JSON.parse(localStorage.getItem('trajets'));
+    console.log(trajets);
+
+
+
+    // Affiche les resultats  : a Modifier
+
+    // function draw(results) {
+    //     $.each(results.journeys, function(i, journey) {
+    //     var str =
+    //         "Depart : " +
+    //         journey.departure_date_time.substring(9, 13) +
+    //         "  /  Arrivée : " +
+    //         journey.arrival_date_time.substring(9, 13) +
+    //         "  /  Durée : " +
+    //         Math.round(journey.duration / 60) +
+    //         " minutes";
+    //     document.getElementById("button").insertAdjacentHTML(
+    //         "afterend",
+    //         "<div id='" + journey.type + "' style='border: solid 1px black; margin: 10px'}>" + str + "</div>"
+    //         );
+    //     str = "";
+    //     $.each(journey.sections, function(i2, section) {
+    //         str = "";
+    //         if (section.mode === "walking") {
+    //             str += "Marche";
+    //         }
+    //         if (section.type === "public_transport") {
+    //             str += section.display_informations.network + " : " + section.display_informations.code;
+    //         }
+    //         document.getElementById(journey.type).insertAdjacentHTML("beforeend", "<div>" + str + "</div>");
+    //     });
+    //     });
+    //     document.getElementById("autocomplete-dataset").value = "";
+    //     document.getElementById("autocomplete-dataset2").value = "";
+    // }
+</script>
