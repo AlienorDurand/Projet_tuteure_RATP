1847937b08b678f025461434286e77c50d823c04
AlienorDurand <32158114+AlienorDurand@users.noreply.github.com>
2019-06-19 11:10:43 +0200

    création chemin trafic

--- View/twitter.php
+++ View/twitter.php
@@ -1,23 +1,14 @@
 <?php
     include_once "header.php" ;
 ?>
-
-
 <div id="partietwitter" > 
-
 <?php
         
-        for($i=0;$i<$total;$i++){
-            echo "<a href='./index.php?ctrl=twitter&action=afficheTwitter2&num=$i'><img src='$tab[$i]'/></a>";
-        }
+    for($i=0;$i<$total;$i++){
+        echo "<a href='./index.php?ctrl=twitter&action=afficheTwitter2&num=$i'><img src='$tab[$i]'/></a>";
+    }
 ?>        
-
-
-    
 </div>
-    
-    
-    
  <?php   
     include_once "footer.php" ;
 ?>
