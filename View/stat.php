d5fca7ae239e0f682b562043ec92ee73dee2ac17
AlienorDurand <32158114+AlienorDurand@users.noreply.github.com>
2019-06-20 09:54:12 +0200

    Update stat.php

--- View/stat.php
+++ View/stat.php
@@ -1,8 +1,16 @@
 <?php
     include_once "header.php" ;
+<<<<<<< HEAD
 ?>
 
 <h1>Stat</h1>
+=======
+?> 
+<div id="partiestat"> 
+    <h2> STATISTIQUES </h2>
+
+</div>
+>>>>>>> master
 <?php
     include_once "footer.php" ;
 ?>
