39fa8ba257de2e8e09b20b5ef5df1c3aad36997d
marcantoine-g <marc-antoine.g@outlook.com>
2019-06-21 09:34:49 +0200

    debut d'affichage

--- View/recherche.php
+++ View/recherche.php
@@ -21,6 +21,7 @@
     var duree = "";
     var modeTransport = "";
     var color="";
+
         $.each(trajets, function(i, trajet) {
             duree = Math.round(trajet.duration / 60) + " minutes";
             document.getElementById("partierecherche").insertAdjacentHTML(
@@ -56,4 +57,84 @@
                 );
             str="";
         });
+    
+
+    var open = false;
+    var listeFleche = document.getElementsByClassName("fleche");
+    var derouleDetail = document.createElement('div');
+    derouleDetail.className = "derouleDetail";
+    derouleDetail.id = "derouleDetail";
+    var departTime = "";
+    var arriveeTime = "";
+
+    $.each(listeFleche, function(i, fleche){
+        fleche.addEventListener('click', afficheDetail);
+        console.log(fleche.id.slice(7));
+    });
+
+    
+    function afficheDetail(e){
+        let idTrajet = e.target.id.slice(7);
+        if(open){
+            open=!open;
+            e.target.parentNode.parentNode.removeChild(derouleDetail);
+        } else {
+            open=!open;
+            e.target.parentNode.insertAdjacentElement(
+                "afterend",
+                derouleDetail
+            );
+
+            $.each(trajets, function(i, trajet){
+                if(trajet.type==idTrajet){
+                    console.log('ok '+trajet.type);      
+                    let longueurTableau  = trajet.sections.length; 
+                    let index = 0;
+                    $.each(trajet.sections, function(j, section){
+                        index++;
+
+                        let departTime = section.departure_date_time.slice(9,13);
+                        departTime = departTime.slice(0,2)+":"+departTime.slice(2,4);
+
+                        let nomStation = "";
+                        if(section.type != "waiting"){
+                            nomStation = section.from.name;
+                        } else {
+                            nomStation = "Patientez";
+                        }
+
+                        derouleDetail.insertAdjacentHTML(
+                            "beforeend",
+                            "<div class='departTime'>"+departTime+"</div>"
+                        );
+                        derouleDetail.insertAdjacentHTML(
+                            "beforeend",
+                            "<div class='nomStation'>"+nomStation+"</div>"
+                        );
+
+                        if(index == longueurTableau){
+                            let arriveeTime = section.arrival_date_time.slice(9,13);
+                            arriveeTime = arriveeTime.slice(0,2)+":"+arriveeTime.slice(2,4);
+                            nomStation = section.to.name;
+
+                            derouleDetail.insertAdjacentHTML(
+                                "beforeend",
+                                "<div class='departTime'>"+arriveeTime+"</div>"
+                            );
+                            derouleDetail.insertAdjacentHTML(
+                                "beforeend",
+                                "<div class='nomStation'>"+nomStation+"</div>"
+                            );
+                        }
+
+
+                    });
+                    
+                }
+            });
+        }
+        
+    }
+
+
 </script>
