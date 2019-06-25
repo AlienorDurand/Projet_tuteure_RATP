7e3c4fe92e69605b9341b9041f217e4e2f6a6096
gaby1994 <gabriel.degez@free.fr>
2019-06-25 14:10:22 +0200

    Update membreController.php

--- Controller/membreController.php
+++ Controller/membreController.php
@@ -1,6 +1,7 @@
 <?php
 
 class membreController {
+<<<<<<< HEAD
    //redirection vers la page de connexion
     public function login(){
         require ('./View/login.php'); 
@@ -15,16 +16,30 @@ class membreController {
     public function doLogin(){
         if(isset($_POST['connexion']) &&  $_POST['connexion'] == 'Connexion') {
             if((isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['password']) && !empty($_POST['password']))) {
+=======
+
+    public function login() {
+        require ('./View/login.php');
+    }
+
+    public function inscription() {
+        require ('./View/inscription.php');
+    }
+
+    public function doLogin() {
+        if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
+            if ((isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['password']) && !empty($_POST['password']))) {
+>>>>>>> master
                 $membre = new Connexion();
-                $data = $membre->getMembre($_POST['mail'],$_POST['password']);        
-                if ($data[0] == 1){
-                session_start();
-                $_SESSION['mail'] = $_POST['mail'];
+                $data = $membre->getMembre($_POST['mail'], $_POST['password']);
+                if ($data[0] == 1) {
+                    session_start();
+                    $_SESSION['mail'] = $_POST['mail'];
                     $nom = $membre->getNom($_SESSION['mail']);
                     $_SESSION['nom'] = $nom['nom'];
                     $prenom = $membre->getPrenom($_SESSION['mail']);
                     $_SESSION['prenom'] = $prenom['prenom'];
-                    $adresse =  $membre->getAdresse($_SESSION['mail']);
+                    $adresse = $membre->getAdresse($_SESSION['mail']);
                     $_SESSION['adresse'] = $adresse['adresse'];
                     $ville = $membre->getVille($_SESSION['mail']);
                     $_SESSION['ville'] = $ville['ville'];
@@ -40,78 +55,77 @@ class membreController {
                     $_SESSION['satisfait'] = $satisfait['satisfait'];
                     $stationPref2 = $membre->getStationPref2($_SESSION['mail']);
                     $_SESSION['stationPreferee2'] = $stationPref2['stationPreferee2'];
-                require('./View/info.php');
-                exit();
+                    require('./View/info.php');
+                    exit();
+                } elseif ($data[0] == 0) {
+                    $erreur = 'Compte non reconnu.';
+                    require('./View/login.php');
+                } else {
+                    $erreur = 'Problème dans la base de données : plusieurs membres ont les mêmes identifiants de connexion.';
+                    require('./View/login.php');
                 }
-                elseif($data[0] == 0) {
-                $erreur = 'Compte non reconnu.';
+            } else {
+                $erreur = 'Au moins un des champs et vide';
                 require('./View/login.php');
-                  }
-                else{
-                $erreur= 'Problème dans la base de données : plusieurs membres ont les mêmes identifiants de connexion.';
-                require('./View/login.php');
-                } 
             }
-            else{
-            $erreur = 'Au moins un des champs et vide';
-            require('./View/login.php');
-            }
-            
         }
     }
+<<<<<<< HEAD
     
     //Inscription du nouveau membre s'il n'est pas deja present dans la base
     public function doInscription(){
-        if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
-	       // on teste l'existence de nos variables. On teste également si elles ne sont pas vides
-	       if ((isset($_POST['nom']) && !empty($_POST['nom'])) && (isset($_POST['prenom']) && !empty($_POST['prenom'])) && (isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['password']) && !empty($_POST['password'])) && (isset($_POST['password_confirm']) && !empty($_POST['password_confirm']))) {
-	           // on teste les deux mots de passworde
-	           if ($_POST['password'] != $_POST['password_confirm']) {
-		          $erreur = 'Les 2 mots de passe sont différents.';
-	           }
-	       else {
-	           $membre = new Connexion();
-               $data = $membre->getMembre($_POST['mail'],$_POST['password']);
+=======
 
-		      if ($data[0] == 0) {
-                  $membre->insertMembre($_POST['nom'],$_POST['prenom'],$_POST['mail'],$_POST['password']);
-                  session_start();
-                  $_SESSION['mail'] = $_POST['mail'];
-                  $nom = $membre->getNom($_SESSION['mail']);
-                $_SESSION['nom'] = $nom['nom'];
-                $prenom = $membre->getPrenom($_SESSION['mail']);
-                $_SESSION['prenom'] = $prenom['prenom'];
-                $adresse =  $membre->getAdresse($_SESSION['mail']);
-                $_SESSION['adresse'] = $adresse['adresse'];
-                $ville = $membre->getVille($_SESSION['mail']);
-                $_SESSION['ville'] = $ville['ville'];
-                $dateNaissance = $membre->getDateNais($_SESSION['mail']);
-                $_SESSION['dateNaissance'] = $dateNaissance['dateNaissance'];
-                $telephone = $membre->getTel($_SESSION['mail']);
-                $_SESSION['telephone'] = $telephone['telephone'];
-                $lignePref = $membre->getLignePref($_SESSION['mail']);
-                $_SESSION['lignePreferee'] = $lignePref['lignePreferee'];
-                $stationPref = $membre->getStationPref($_SESSION['mail']);
-                $_SESSION['stationPreferee'] = $stationPref['stationPreferee'];
-                $satisfait = $membre->getSatisfait($_SESSION['mail']);
-                $_SESSION['satisfait'] = $satisfait['satisfait'];
-                $stationPref2 = $membre->getStationPref2($_SESSION['mail']);
-                $_SESSION['stationPreferee2'] = $stationPref2['stationPreferee2'];
-                  require('./View/info.php');
-                  exit();
-		      }
-		      else {
-		          $erreur = 'Un membre possède déjà ce mail.';
-                  require('./View/inscription.php');
-		      }
-	       }
-	       }
-	   else {
-	       $erreur = 'Au moins un des champs est vide.';
-           require('./View/inscription.php');
-	   }
-       }
+    public function doInscription() {
+>>>>>>> master
+        if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
+            // on teste l'existence de nos variables. On teste également si elles ne sont pas vides
+            if ((isset($_POST['nom']) && !empty($_POST['nom'])) && (isset($_POST['prenom']) && !empty($_POST['prenom'])) && (isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['password']) && !empty($_POST['password'])) && (isset($_POST['password_confirm']) && !empty($_POST['password_confirm']))) {
+                // on teste les deux mots de passworde
+                if ($_POST['password'] != $_POST['password_confirm']) {
+                    $erreur = 'Les 2 mots de passe sont différents.';
+                } else {
+                    $membre = new Connexion();
+                    $data = $membre->getMembre($_POST['mail'], $_POST['password']);
+
+                    if ($data[0] == 0) {
+                        $membre->insertMembre($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['password']);
+                        session_start();
+                        $_SESSION['mail'] = $_POST['mail'];
+                        $nom = $membre->getNom($_SESSION['mail']);
+                        $_SESSION['nom'] = $nom['nom'];
+                        $prenom = $membre->getPrenom($_SESSION['mail']);
+                        $_SESSION['prenom'] = $prenom['prenom'];
+                        $adresse = $membre->getAdresse($_SESSION['mail']);
+                        $_SESSION['adresse'] = $adresse['adresse'];
+                        $ville = $membre->getVille($_SESSION['mail']);
+                        $_SESSION['ville'] = $ville['ville'];
+                        $dateNaissance = $membre->getDateNais($_SESSION['mail']);
+                        $_SESSION['dateNaissance'] = $dateNaissance['dateNaissance'];
+                        $telephone = $membre->getTel($_SESSION['mail']);
+                        $_SESSION['telephone'] = $telephone['telephone'];
+                        $lignePref = $membre->getLignePref($_SESSION['mail']);
+                        $_SESSION['lignePreferee'] = $lignePref['lignePreferee'];
+                        $stationPref = $membre->getStationPref($_SESSION['mail']);
+                        $_SESSION['stationPreferee'] = $stationPref['stationPreferee'];
+                        $satisfait = $membre->getSatisfait($_SESSION['mail']);
+                        $_SESSION['satisfait'] = $satisfait['satisfait'];
+                        $stationPref2 = $membre->getStationPref2($_SESSION['mail']);
+                        $_SESSION['stationPreferee2'] = $stationPref2['stationPreferee2'];
+                        require('./View/info.php');
+                        exit();
+                    } else {
+                        $erreur = 'Un membre possède déjà ce mail.';
+                        require('./View/inscription.php');
+                    }
+                }
+            } else {
+                $erreur = 'Au moins un des champs est vide.';
+                require('./View/inscription.php');
+            }
+        }
     }
+<<<<<<< HEAD
     
     //redirection vers la page avec les infos affichees par l'ecran
     public function pageAccueilMembre(){
@@ -120,13 +134,21 @@ class membreController {
     
     //acces aux donnees personnelles de l'utilisateur connecte
     public function infoPerso(){
+=======
+
+    public function pageAccueilMembre() {
+        require('./View/info.php');
+    }
+
+    public function infoPerso() {
+>>>>>>> master
 
         $membre = new Connexion();
         $nom = $membre->getNom($_SESSION['mail']);
         $_SESSION['nom'] = $nom['nom'];
         $prenom = $membre->getPrenom($_SESSION['mail']);
         $_SESSION['prenom'] = $prenom['prenom'];
-        $adresse =  $membre->getAdresse($_SESSION['mail']);
+        $adresse = $membre->getAdresse($_SESSION['mail']);
         $_SESSION['adresse'] = $adresse['adresse'];
         $ville = $membre->getVille($_SESSION['mail']);
         $_SESSION['ville'] = $ville['ville'];
@@ -142,9 +164,10 @@ class membreController {
         $_SESSION['satisfait'] = $satisfait['satisfait'];
         $stationPref2 = $membre->getStationPref2($_SESSION['mail']);
         $_SESSION['stationPreferee2'] = $stationPref2['stationPreferee2'];
-        
+
         require('./View/info.php');
     }
+<<<<<<< HEAD
     
     //redirection vers la page qui affiche le formulaire afin de pouvoir modifier les infos de l'utilisateur connecte
     public function updateInfoPerso(){
@@ -155,23 +178,35 @@ class membreController {
     //sauvegarde des données personnelles de l'utilisateur connecté
     public function sauvInforPerso(){
         
+=======
+
+    public function updateInfoPerso() {
+
+        require('./View/modifMembre.php');
+    }
+
+    public function sauvInforPerso() {
+
+>>>>>>> master
         $membre2 = new Connexion();
-        $membre2->update($_POST['nom'],$_POST['prenom'],$_POST['adresse'],$_POST['ville'],$_POST['dateNaissance'],$_POST['telephone'],$_POST['lignePreferee'],$_POST['stationPreferee'],$_POST['satisfait'],$_POST['stationPreferee2'],$_SESSION['mail']);
+        $mdp = md5($_POST['password']);
+        $membre2->update($_POST['mail'],$mdp,$_POST['nom'], $_POST['prenom'], $_POST['adresse'], $_POST['ville'], $_POST['dateNaissance'], $_POST['telephone'], $_POST['lignePreferee'], $_POST['stationPreferee'], $_POST['satisfait'], $_POST['stationPreferee2'], $_SESSION['mail']);
         
         $membre = new Connexion();
         
+        $_SESSION['mail'] = $_POST['mail'];
         $nom = $membre->getNom($_SESSION['mail']);
         $_SESSION['nom'] = $nom['nom'];
         $prenom = $membre->getPrenom($_SESSION['mail']);
-        $_SESSION['prenom'] = $prenom['prenom'];    
-        $adresse =  $membre->getAdresse($_SESSION['mail']);
+        $_SESSION['prenom'] = $prenom['prenom'];
+        $adresse = $membre->getAdresse($_SESSION['mail']);
         $_SESSION['adresse'] = $adresse['adresse'];
         $ville = $membre->getVille($_SESSION['mail']);
-        $_SESSION['ville'] = $ville['ville'];      
+        $_SESSION['ville'] = $ville['ville'];
         $dateNaissance = $membre->getDateNais($_SESSION['mail']);
-        $_SESSION['dateNaissance'] = $dateNaissance['dateNaissance'];     
+        $_SESSION['dateNaissance'] = $dateNaissance['dateNaissance'];
         $telephone = $membre->getTel($_SESSION['mail']);
-        $_SESSION['telephone'] = $telephone['telephone'];      
+        $_SESSION['telephone'] = $telephone['telephone'];
         $lignePref = $membre->getLignePref($_SESSION['mail']);
         $_SESSION['lignePreferee'] = $lignePref['lignePreferee'];
         $stationPref = $membre->getStationPref($_SESSION['mail']);
@@ -183,14 +218,16 @@ class membreController {
 
         require('./View/info.php');
     }
+<<<<<<< HEAD
     
     //Deconnexion du membre
     public function deconnexion(){
+=======
+
+    public function deconnexion() {
+>>>>>>> master
         session_unset();
-        require('./View/default.php'); 
+        require('./View/default.php');
     }
 
-    
-    
 }
-
