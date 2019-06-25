ffcfdd808b218b2ba2d3903409a623afe731b186
marcantoine-g <marc-antoine.g@outlook.com>
2019-06-25 16:19:29 +0200

    Mise en place de l'autocomplete ainsi que du style associé

--- View/modifMembre.php
+++ View/modifMembre.php
@@ -2,7 +2,7 @@
     include_once "header.php" ;
 ?>     
 
-    <form id="modifinfocompte" action="./index.php?ctrl=membre&action=sauvInforPerso" method="post" >
+    <form id="modifinfocompte" action="./index.php?ctrl=membre&action=sauvInforPerso" method="post" name="modifinfocompte">
 
         <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Mail </span> <span class="col-8" > <input type="text" name="mail" value="<?php echo $_SESSION['mail']; ?>" placeholder="<?php echo $_SESSION['mail']; ?>"> </span> </div> </div><br />
         
@@ -21,17 +21,119 @@
         <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Téléphone </span> <span class="col-8" > <input type="text" name="telephone" value="<?php echo $_SESSION['telephone'];?>" placeholder="<?php echo ($_SESSION['telephone']); ?>"> </span> </div> </div><br />
         
         <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Ligne préférée </span> <span class="col-8" > <input type="text" name="lignePreferee" value="<?php echo ($_SESSION['lignePreferee']); ?>" placeholder="<?php echo ($_SESSION['lignePreferee']); ?>"> </span> </div> </div><br />
-        
-        <div class="row"> <div class="formmodifinfo"> <span class="col-4" >Station préférée </span> <span class="col-8" > <input type="text" name="stationPreferee" value="<?php echo($_SESSION['stationPreferee']); ?>" placeholder="<?php echo($_SESSION['stationPreferee']);?>"> </span> </div> </div><br />
-        
-        <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Satisfait ? </span> <span class="col-8" > <input type="text" name="satisfait" value="<?php echo ($_SESSION['satisfait']); ?>" placeholder="<?php echo ($_SESSION['satisfait']);?>"> </span> </div> </div><br />
-        
-        <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Station préférée 2 </span> <span class="col-8" > <input type="text" name="stationPreferee2" value="<?php echo ($_SESSION['stationPreferee2']); ?>" placeholder="<?php echo ($_SESSION['stationPreferee2']); ?>"> </span> </div> </div><br />
-        
-        <input id="bouttonmodif" type="submit" name="ajouter" value="Ajouter">
+        <!-- ICI -->
+        <div class="row"> <div class="formmodifinfo"> <span class="col-4" >Station préférée </span> <span class="col-8 spanStation" > <input id="stationPref1" type="search" name="stationPreferee" value="<?php echo($_SESSION['stationPreferee']); ?>" placeholder="<?php echo($_SESSION['stationPreferee']);?>"> </span> </div> </div><br />
+        <!--  -->
+        <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Satisfait ? </span> <span class="col-8"  > <input type="text" name="satisfait" value="<?php echo ($_SESSION['satisfait']); ?>" placeholder="<?php echo ($_SESSION['satisfait']);?>"> </span> </div> </div><br />
+        <!-- ICI -->
+        <div class="row"> <div class="formmodifinfo"> <span class="col-4" > Station préférée 2 </span> <span class="col-8 spanStation"> <input id="stationPref2" type="search" name="stationPreferee2" value="<?php echo ($_SESSION['stationPreferee2']); ?>" placeholder="<?php echo ($_SESSION['stationPreferee2']); ?>"> </span> </div> </div><br />
+        <!--  -->
+        <input id="bouttonmodif" type="button" name="ajouter" value="Ajouter" onclick="verifClick()">
         
     </form>
-    <?php
+
+<script>
+
+var placesDataset = placesAutocompleteDataset({
+    appId: "plZ6ZC3CU96B",
+    apiKey: "c87e3c4fbfc80ea56598fe67a55f8701",
+    algoliasearch: algoliasearch,
+    templates: {
+      header: '<div class="ad-example-header">Suggestions</div>',
+      suggestion: function(suggestion) {
+        let type = "";
+        if (suggestion.type === "trainStation") {
+          type = "<i>TchouTchou ! </i>";
+        }
+        if (suggestion.type === "city" || suggestion.type === "address") {
+          type = "<i>City !</i>";
+        }
+        return type + " " + suggestion.value;
+      }
+    },
+    language: "fr",
+    countries: ["fr"],
+    useDeviceLocation: true,
+    aroundRadius: 70000,
+    hitsPerPage: 5,
+    type: 'trainStation'
+  });
+
+    var autocompleteStationPref1 = autocomplete(
+            stationPref1,
+            {
+                hint: false,
+                debug: true,
+                cssClasses: {prefix: "ac-input"}, // Les classes css 
+                autoselect: true,
+                autoselectOnBlur: true,
+                openOnFocus : true,
+                minLength: 3
+            },
+            [placesDataset]
+    );
+
+    var autocompleteStationPref2 = autocomplete(
+            stationPref2,
+            {
+                hint: false,
+                debug: true,
+                // cssClasses: {prefix: "ac-input"}, // Les classes css 
+                autoselect: true,
+                autoselectOnBlur: true,
+                openOnFocus : true,
+                minLength: 3
+            },
+            [placesDataset]
+    );
+
+    var autocompleteChangeEvents = ["selected", "autocompleted"];
+
+    var boolPref1 = false;
+    var boolPref2 = false;
+
+    autocompleteChangeEvents.forEach(function (eventName) {
+        autocompleteStationPref1.on("autocomplete:" + eventName, function (
+            event,
+            suggestion,
+            datasetName
+        ) {
+            if(eventName == "selected"){
+                console.log(suggestion);
+                boolPref1 = true;
+                nomStation1 = suggestion.name+" ("+suggestion.city+")";
+                autocompleteStationPref1.autocomplete.setVal(nomStation1);
+            }
+        });
+    });
+
+    autocompleteChangeEvents.forEach(function (eventName) {
+        autocompleteStationPref2.on("autocomplete:" + eventName, function (
+            event,
+            suggestion,
+            datasetName
+        ) {
+            if(eventName == "selected"){
+                console.log(suggestion);
+                boolPref2 = true;
+                nomStation2 = suggestion.name+" ("+suggestion.city+")";
+                autocompleteStationPref2.autocomplete.setVal(nomStation2);
+            }
+        });
+    });
+
+    // Modifier l'input pour vérifier si les valeurs sont a true avant d'accepter (la personne doit choisir parmis les suggestions)
+    function verifClick(){
+        if (boolPref1 && boolPref2){
+            document.forms.modifinfocompte.submit();
+        } else {
+            alert("Il faut choisir une station à partir des suggestions");
+        }
+    }
+
+</script>
+
+<?php
     if (isset($erreur)) echo '<br />',$erreur;
     include_once "header.php" ;
 ?>
